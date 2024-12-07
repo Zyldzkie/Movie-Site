eb8fa92fdf05530cb1d13f49c3cd37cbb0cc721b
Werdddd <148359408+Werdddd@users.noreply.github.com>
2024-12-07 15:32:42 +0800

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login/Login';
 import Sidebar from './Components/Sidebar';
@@ -16,10 +17,25 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />Y
           {/* Add other protected routes here */}
+=======
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage'; // Import the HomePage component
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        {/* Routes define which component to load based on the URL */}
+        <Routes>
+          {/* Default route loads the HomePage */}
+          <Route path="/" element={<HomePage />} />
+>>>>>>> Stashed changes
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
