7715a0ca05656de115b94800f302a634aae24239
Zyldzkie <Zydtube888@gmail.com>
2024-12-13 15:51:02 +0800

    [Update] Roles

--- backend/codes/generate_tables.php
+++ backend/codes/generate_tables.php
@@ -30,7 +30,7 @@ $table_queries = [
         email VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
         name VARCHAR(255) NOT NULL,
-        role ENUM('Admin', 'Editor', 'Viewer') NOT NULL
+        role ENUM('Admin', 'Viewer') NOT NULL
     )",
     
     "CREATE TABLE IF NOT EXISTS Movies (
