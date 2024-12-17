3af9b6bc987612353e8c6f737bd221d4ef53ac3d
Zyldzkie <88105329+Zyldzkie@users.noreply.github.com>
2024-12-17 09:54:07 +0800

    [Feature] Add admin edit

--- backend/codes/routing.php
+++ backend/codes/routing.php
@@ -19,6 +19,9 @@ switch($user_link){
     case "/get_movies":
         require("restapi/get_movies.php");
         break;
+    case "/admin_edit":
+        require("restapi/admin_edit.php");
+        break;
     default:
         http_response_code(404);
         break;
